1bd57183aca0d5aee22a8cf79b828f07e92b603a
Fede Dotto <fedexd@hotmail.com>
2021-11-13 22:36:34 +0100

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from './components/App'
+
+// En Codigo JSX escribimos directamente las etiquetas
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
